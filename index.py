102358124b4c3b6492369b067508667b110d1b17
Ankit <ankitgdes@gmail.com>
2021-10-14 12:49:19 +0530

    Fix: Seek added

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
+	rawCapture.seek(0)
 	# if the `q` key was pressed, break from the loop
 	if key == ord("w"):
 		if camera.contrast < 100:
